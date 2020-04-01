2ac781e53ac1f835711ba2997d85aa143ba3f15c
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-04-01 14:31:52 -0600

    rendering stock data on the front-end.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,7 @@
 class StocksController < ApplicationController
   def search
     @stock = Stock.new_lookup(params[:stock])
-    render json: @stock
+    render 'users/portfolio'
   end
 
   private
