49996012d2aee1517545aeb68f5bc05d041e3170
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-04-01 14:18:42 -0600

    completed price lookup.

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,11 @@
+class StocksController < ApplicationController
+  def search
+    stock = Stock.new_lookup(params[:stock])
+    render json: stock
+  end
+
+  private
+  def search_params
+    params.require(:stock).permit(:stock)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
   get 'portfolio', to: "users#portfolio"
+  get 'search_stock', to: 'stocks#search'
+
+
 end
