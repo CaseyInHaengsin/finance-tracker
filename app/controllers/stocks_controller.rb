62e422b1dc3956a9b66fae4e319b7502e07ccbd8
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-04-02 06:23:55 -0600

    added error handling for empty value in search.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,13 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      render 'users/portfolio'
+
+      else
+    flash[:alert] = "Please enter a symbol to search"
+    redirect_to portfolio_path
+    end
   end
 
   private
