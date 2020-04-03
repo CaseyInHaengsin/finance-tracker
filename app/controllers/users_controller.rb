3a138f27e1f45facfc75cae6dc9f5238f32c1c86
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-04-03 06:41:09 -0600

    added stock tracking.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def portfolio
+    #Note - Devise uses a curent_user and user_signed_in? method
+    @tracked_stocks = current_user.stocks
   end
 end
