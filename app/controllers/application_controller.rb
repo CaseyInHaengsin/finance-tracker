f2bbcec3d43c62b354eeaba4121a74626d23c135
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-04-12 15:04:17 -0600

    customized devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
