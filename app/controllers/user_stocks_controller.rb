780dc3c55db5eebdc8f97f2573db41ccafc210dd
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-04-12 11:37:10 -0600

    adding remove functionality.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,6 +11,14 @@ class UserStocksController < ApplicationController
       redirect_to portfolio_path
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+    redirect_to portfolio_path
+  end
+
 
   private
 
